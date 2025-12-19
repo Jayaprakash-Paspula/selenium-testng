378b6e5571da3c983ab9422b33413222d40d4c66
Jayaprakash-Paspula <134996192+Jayaprakash-Paspula@users.noreply.github.com>
2025-12-19 12:06:12 +0530

    Create GoogleSearchTest.java

---
+++ src/test/java/tests/GoogleSearchTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleSearchTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void verifyGoogleTitle() {
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Google"), "Title does not contain Google");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}

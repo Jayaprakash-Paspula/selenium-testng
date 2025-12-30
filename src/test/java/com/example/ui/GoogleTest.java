04dc297e391a442b8f3ca941c0bdc8911745b7fb
Jayaprakash-Paspula <134996192+Jayaprakash-Paspula@users.noreply.github.com>
2025-12-30 21:27:06 +0530

    Add Google homepage title verification test
    This test verifies that the Google homepage title contains 'Google'.

---
+++ src/test/java/com/example/ui/GoogleTest.java
@@ -0,0 +1,38 @@
+package com.example.ui;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void verifyGoogleTitle() {
+        driver.get("https://www.google.com");
+        Assert.assertTrue(driver.getTitle().contains("Google"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
